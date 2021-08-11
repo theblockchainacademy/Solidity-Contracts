edb2e23a419506ca1ee9d1b769edfba6a60b39c8
theblockchainacademy <88111541+theblockchainacademy@users.noreply.github.com>
2021-08-11 14:16:10 -0400

    Create MyContract1.sol
    Add new file

---
+++ MyContract1.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.5.2;
+import "remix_tests.sol"; // this import is automatically injected by Remix.
+import "/tests/MyContract1_test.sol"; // this import is automatically injected by Remix.
+
+contract MyContract1 {
+    string public value;
+    string public myVal;
+    int256 public myInt;
+    string public str1;
+ 
+    
+    constructor() public {
+        
+        set("Good","Day");
+        
+        value = "myValue";
+        myVal = "myString";
+    }
+    
+    function hint (int256 i) public returns (int256 fullInt){
+        
+        myInt=i;
+        fullInt = myInt + 10;
+        return fullInt;
+          
+    }
+    
+    function get() public  view returns(string memory , string memory) {
+        return (value, myVal);
+        
+    }
+    
+     function myGet() public returns(string memory) {
+        str1 = myVal;
+        return str1;
+        
+    }
+    
+    function set(string memory _value, string memory _MyVal1) public {
+        value = _value;
+        myVal = _MyVal1;  
+    }
+    
+}
